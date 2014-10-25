ec381a21b35351dc62966838c9b73889adf87687
Tom Wiesing <tkw01536@gmail.com>
2014-10-25 12:23:09 +0200

    Reorganisation & added doc

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var
     express = require('express'),
     winston = require('winston'),
 
-    ConnectionHost = require('./lib/ConnectionHost.js').ConnectionHost;
+    ConnectionHost = require('./lib/server/ConnectionHost.js').ConnectionHost;
 
 //Winston: Logging
 winston.remove(winston.transports.Console);
@@ -31,8 +31,22 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
-app.use('/lib', express.static(__dirname + '/bower_components'))
+
+// Route: /lib/poke-client
+app.use('/lib/poke-client/js/shared', express.static(__dirname + '/lib/shared'));
+app.use('/lib/poke-client/js', express.static(__dirname + '/lib/client'));
+app.use('/lib/poke-client/css', express.static(__dirname + '/lib/components/css/poke-client'));
+
+//Route: /lib/press-start-2p
+app.use('/lib/press-start-2p', express.static(__dirname + '/lib/components/css/press-start-2p'));
+
+//Route: /lib
+app.use('/lib', express.static(__dirname + '/bower_components'));
+
+//Route: /
 app.use(express.static(__dirname + '/static'));
+
+//Listen on the HTTP Server
 http.listen(port, function(){
   winston.info('listening on *:'+port.toString());
 });
